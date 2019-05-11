250c2b27c5e23b8d1855f8de261e825718c72930
badosz0 <badoszk@gmail.com>
2019-05-11 11:16:01 +0200

    better starboard

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -12,7 +12,7 @@ module.exports = async (reaction, user) => {
     const hasContent = reaction.message.content && reaction.message.content.length || reaction.message.attachments.size && reaction.message.attachments.first().height;
     const total = reaction.message.reactions.find(r => r.emoji.name === '⭐') ? reaction.message.reactions.find(r => r.emoji.name === '⭐').count : 0 ;
     const fetch = await starChannel.fetchMessages({ limit: 100 });
-    const starMsg = fetch.find(m => m.embeds[0] && m.embeds[0].footer && m.embeds[0].footer.text.startsWith("⭐") && m.embeds[0].footer.text.endsWith(reaction.message.id));
+    const starMsg = fetch.find(m => m.content.startsWith("⭐") && m.embeds[0] && m.embeds[0].footer && m.embeds[0].footer.text.endsWith(reaction.message.id));
     if (hasContent && total >= star.number) {
         let image = '';
         if (reaction.message.attachments.size) {
@@ -24,21 +24,21 @@ module.exports = async (reaction, user) => {
             if (reaction.message.guild.channels.has(star.channel)) {
                 if(starMsg){
                     const embed = new Discord.RichEmbed()
-                        .setDescription(`**Author: **${reaction.message.author.tag}\n**Channel: **${reaction.message.channel.toString()}\n**Message:**\n${reaction.message.content}\n\n[[link]](https://discordapp.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id})`)
-                        .setFooter(`⭐${reaction.message.reactions.get("⭐").count} | ID: ${reaction.message.id}`)
+                        .setDescription(`${reaction.message.content}\n\n[[link]](https://discordapp.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id})`)
+                        .setFooter(`ID: ${reaction.message.id}`)
+                        .setAuthor(reaction.message.author.tag, reaction.message.author.displayAvatarURL)
                         .setImage(image)
-                        .setThumbnail(reaction.message.author.displayAvatarURL)
                         .setTimestamp();
                     const oldMsg = await starChannel.fetchMessage(starMsg.id);
-                    await oldMsg.edit({ embed })
+                    await oldMsg.edit(`⭐${reaction.message.reactions.get("⭐").count} ${reaction.message.channel}`,{ embed })
                 }else{
                     const embed = new Discord.RichEmbed()
-                    .setDescription(`**Author: **${reaction.message.author.tag}\n**Channel: **${reaction.message.channel.toString()}\n**Message:**\n${reaction.message.content}\n\n[[link]](https://discordapp.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id})`)
-                    .setFooter(`⭐${reaction.message.reactions.get("⭐").count} | ID: ${reaction.message.id}`)
+                    .setDescription(`${reaction.message.content}\n\n[[link]](https://discordapp.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id})`)
+                    .setFooter(`ID: ${reaction.message.id}`)
                     .setImage(image)
-                    .setThumbnail(reaction.message.author.displayAvatarURL)
+                    .setAuthor(reaction.message.author.tag, reaction.message.author.displayAvatarURL)
                     .setTimestamp();
-                    reaction.message.guild.channels.get(star.channel).send(embed);
+                    reaction.message.guild.channels.get(star.channel).send(`⭐${reaction.message.reactions.get("⭐").count} ${reaction.message.channel}`,{embed});
                 }
             }
         }
