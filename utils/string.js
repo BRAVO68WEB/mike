3e393f8f767c642df38ffa2500b417234519caa5
badosz0 <badoszk@gmail.com>
2019-05-15 20:27:21 +0200

    toTitleCase() prototype

--- utils/string.js
+++ utils/string.js
@@ -7,3 +7,14 @@ exports.sparse = async (string) => {
   });
   return output
 }
+
+String.prototype.toTitleCase = function() {
+  let newstr = this.split(' ');
+  for (let i = 0; i < newstr.length; i++) {
+    if (newstr[i] === '') continue;
+    let copy = newstr[i].substring(1).toLowerCase();
+    newstr[i] = newstr[i][0].toUpperCase() + copy;
+  }
+  newstr = newstr.join(' ');
+  return newstr;
+};
