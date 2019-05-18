d474b24fb63a680b945f6a111d5e992a131a01dc
badosz0 <badoszk@gmail.com>
2019-05-18 13:44:53 +0200

    WHY i made this so

--- managers/CommandsLoader.js
+++ managers/CommandsLoader.js
@@ -4,24 +4,19 @@ const fs = require('fs');
 class CommandsLoader {
     constructor(Mike) {
         Mike.commands = new Collection()
-        
+
         fs.readdirSync('./commands/').forEach(category => {
-        
+
             const commandFiles = fs.readdirSync(`./commands/${category}`).filter(file => file.endsWith('.js'));
-            const categoryFiles = fs.readdirSync('./commands/').filter(file => file);
-            
+
             for (const file of commandFiles) {
-                
+
                 const command = require(`../commands/${category}/${file}`);
 
                 Mike.commands.set(command.data.triggers, command, category);
             }
 
-            for (const file of categoryFiles) {
-             
-                const category = file.split('.')[0];
-                Mike.categories.push(category);
-            }
+            Mike.categories.push(category);
 
         });
     }
