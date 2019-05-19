baad028975e6beca6c9133a5b922715ec9e2bbb6
badosz0 <badoszk@gmail.com>
2019-05-19 11:08:17 +0200

    better prune

--- commands/moderation/prune.js
+++ commands/moderation/prune.js
@@ -3,7 +3,20 @@ exports.output = async ({message, args}) => {
         if (args[0] < 1 || args[0] > 100) {
             return Mike.exec.error(message, 'Only positive numbers ranged from 1 - 100 are accepted!')
         }
-        const fetched = await message.channel.fetchMessages({ limit: parseInt(args[0])});
+        let fetched = await message.channel.fetchMessages({ limit: parseInt(args[0]) && parseInt(args[0]) < 100 ? parseInt(args[0]) : 100});
+        let user;
+        if (message.mentions.users.size) {
+          user = message.mentions.users.first();
+        }
+
+        if (user) {
+          fetched = fetched.filter(message => message.author.id === user.id);
+        }
+
+        if (args.includes(`--bots`)) {
+          fetched = fetched.filter(message => message.author.bot);
+        }
+
         message.channel.bulkDelete(fetched);
         Mike.exec.snap(message, `${message.author.username} has deleted ${fetched.size} message(s)!`).then(msg => { msg.delete(6000); });
     }
@@ -13,13 +26,10 @@ exports.data = {
     triggers: ['prune', 'purge', 'clear', 'clean', 'delete'],
     description: 'Prunes messages.',
     usage: [
-        '{prefix}{command} <number>'
-    ],
-    args: [
-        {
-            'type':'int',
-            'name':'number'
-        },
+        '{prefix}{command} [number]',
+        '{prefix}{command} [number]',
+        '{prefix}{command} [number] [--bots]',
+        '{prefix}{command} [number] [mention]'
     ],
     userPerms: [
         "MANAGE_MESSAGES"
