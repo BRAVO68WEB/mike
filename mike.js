1129fc5bdcc2db0b670c6370ade8cf25dba04995
badosz0 <badoszk@gmail.com>
2019-06-05 20:31:49 +0200

    main struct

--- mike.js
+++ mike.js
@@ -1,15 +1,27 @@
-const { Client } = require('discord.js');
-class Mike extends Client {
-    constructor(bot) {
-        super(bot)
+const { Client } = require('discord.js')
+const os = require('os')
 
-        global.Mike = this
+class Mike extends Client {
+  constructor(bot) {
+    super(bot)
+
+    global.Mike = this
+
+    this.tokens = require('./files/tokens.json')
+
+    if (os.hostname() == 'badosz-pc') {
+      this.login(this.tokens.beta)
+    } else {
+      this.login(this.tokens.beta)
+    }
+
+  }
 }
 
-const Core = new Mike(
+const Core = new Mike (
   {
-      disabledEvents: [
-          'TYPING_START',
-      ]
+    disabledEvents: [
+      'TYPING_START',
+    ]
   }
 )
