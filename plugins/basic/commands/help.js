7f717eaa86240fe31373193d410f19cc21ee20f9
badosz0 <badoszk@gmail.com>
2019-06-06 18:14:57 +0200

    commands in help

--- plugins/basic/commands/help.js
+++ plugins/basic/commands/help.js
@@ -2,6 +2,20 @@ exports.output = async ({message, args}) => {
 
   const plugin = Mike.plugins.find(plugin => plugin.id.includes(args[0]))
   if (plugin) {
+    let commands = {
+      normal:   [],
+      voters:   [],
+      donators: []
+    }
+    for (const command of plugin.commands) {
+      if (command.data.voter) {
+        commands.voters.push(`\`${command.data.triggers[0]}\``)
+      } else if (command.data.donator) {
+        commands.donators.push(`\`${command.data.triggers[0]}\``)
+      } else {
+        commands.normal.push(`\`${command.data.triggers[0]}\``)
+      }
+    }
     return Mike.models.snap({
       object: message,
       message: `**${plugin.name}**
@@ -11,7 +25,13 @@ exports.output = async ({message, args}) => {
 
                 Can Be Disabled: \`${plugin.canBeDisabled ? `Yes` : 'No'}\`
                 Can Be Hidden: \`${plugin.canBeHidden ? `Yes` : 'No'}\`
-                
+
+                Commands:
+                ${commands.normal.join(', ')}
+
+                ${commands.voters.length > 0 ? `Voters commands:\n` + commands.voters.join(', ') : ``}
+
+                ${commands.donators.length > 0 ? `Donators commands:\n` + commands.donators.join(', ') : ``}
                 `,
     })
   }
@@ -29,5 +49,6 @@ exports.output = async ({message, args}) => {
 
 exports.data = {
     triggers: ['help'],
-    description: 'Shows help message.'
+    description: 'Shows help message.',
+    donator: true
 }
