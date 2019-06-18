145b88e2516aa0c33b5103bf799b6ec0165ac835
badosz0 <badoszk@gmail.com>
2019-06-18 22:38:49 +0200

    rules command

---
+++ plugins/dev/commands/rules.js
@@ -0,0 +1,32 @@
+exports.output = async ({message, args}) => {
+  const rules = `
+  **Server Rules**
+
+  **•** Do not use bots outside of <#518788823178805259>.
+  **•** Do not spam, advertise anything.
+  **•** Do not attack or harass anyone.
+  **•** Do not post NSFW content.
+  **•** Do not message <@214858075650260992> unless he have told you that you can.
+  **•** Do not mention users without very good reason.
+  **•** Do not use @everyone or @here.
+
+
+  \u200B
+`
+
+
+  Mike.models.snap({
+    object: message,
+    message: rules,
+    footer: `Updated: ${new Date().getDate()}/${new Date().getMonth()}/${new Date().getFullYear()}`
+  })
+
+  message.delete()
+}
+
+
+exports.data = {
+  triggers: ['rules'],
+  description: 'Sends rules to channel.',
+  developer: true,
+}
