8194b7ddc19b41cc53eee9f37681166ddbe6cba2
badosz0 <badoszk@gmail.com>
2019-06-21 23:29:20 +0200

    regex util

---
+++ utils/regex.js
@@ -0,0 +1,5 @@
+exports.link = async (text) => {
+  const match = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#/%=~_|$?!:,.]*\)|[A-Z0-9+&@#/%=~_|$])/i.exec(text)
+  if (match) return true
+  else return false
+}
