8bb21d7c18d820d73bd7ba4906f108f650b93f0a
Nimplex <frekerhugug@gmail.com>
2019-07-06 22:27:23 +0200

    Better Userinfo

--- plugins/info/commands/userinfo.js
+++ plugins/info/commands/userinfo.js
@@ -1,15 +1,20 @@
 exports.output = async ({message, args}) => {
   const user = await Mike.utils.users.search(message, args[0])
   const member = await message.guild.fetchMember(user);
+  const isBot = {
+    "true": `${Mike.customEmojis.markYes} Yes`,
+    "false": `${Mike.customEmojis.markNo} No`
+  }
   Mike.models.mult({
     object: message,
     fields: [
       ["Username", user.tag, true],
       ["Nick", user.nick ? user.nick : user.username, true],
+      ['Bot?',isBot[member.user.bot],true],
       ["ID", user.id, true],
-      ["Status", user.presence.status.replace(/online/g, "Online").replace(/idle/g, "Idle").replace(/dnd/g, "Do Not Disturb").replace(/offline/g, "Offline"), true],
+      ["Status", user.presence.status.replace(/online/g, `${Mike.customEmojis.statusOnline} Online`).replace(/idle/g, `${Mike.customEmojis.statusIdle} Idle`).replace(/dnd/g, `${Mike.customEmojis.statusDnd} Do Not Disturb`).replace(/offline/g, `${Mike.customEmojis.statusOffline} Offline`), true],
       ["In", user.presence.game ? `${user.presence.game.name}\n\`${user.presence.game.details ? user.presence.game.details : '--'}\`\n\`${user.presence.game.state ? user.presence.game.state : '--'}\`` : "Nothing", true],
-      ["Muted", member.serverMute ? `Yes` : `No`, true],
+      ["Muted", member.serverMute ? `${Mike.customEmojis.markYes} Yes` : `${Mike.customEmojis.markNo} No`, true],
       ["Color", member.displayHexColor, true],
       ["Highest Role", member.highestRole.name.replace(/@everyone/g, '-'), true],
       ["Joined Guild", member.joinedAt.toUTCString(), true],
@@ -20,7 +25,7 @@ exports.output = async ({message, args}) => {
   console.log(user.presence)
 }
 exports.data = {
-  triggers: ['userinfo'],
+  triggers: ['userinfo','user'],
   description: 'Shows user info.',
   usage: [
       '{prefix}{command} [mention]',
