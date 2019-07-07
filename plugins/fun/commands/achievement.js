158a31223b45b99da3644d2200a70016b23d5191
Nimplex <frekerhugug@gmail.com>
2019-07-07 16:03:56 +0200

    Added achievement command

---
+++ plugins/fun/commands/achievement.js
@@ -0,0 +1,27 @@
+const imageType = require('image-type')
+const { Attachment } = require('discord.js')
+exports.output = async ({message, args}) => {
+  Mike.http.get(`https://www.minecraftskinstealer.com/achievement/a.php?i=13&h=Achievement%20unlocked&t=` + args.join(" ")).then(async response => {
+    const type = imageType(response.body)
+    const file = new Attachment(response.body, `file.${type ? type.ext : 'png'}`)
+    Mike.models.snap({
+      object: message,
+      file: file,
+      message:'',
+      image: `attachment://file.${type ? type.ext : 'png'}`,
+    })
+  })
+}
+exports.data = {
+  triggers: ['achievement'],
+  description: 'None',
+  usage: [
+    '{prefix}{command} <text>'
+  ],
+  args: [
+    {
+      'type':'any',
+      'name':'text'
+    }
+  ]
+}
