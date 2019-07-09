17d0f592e1dbbffca6c39b983e671b1ee5824f8b
badosz0 <badoszk@gmail.com>
2019-07-09 22:39:52 +0200

    code cleanup

--- plugins/info/commands/avatar.js
+++ plugins/info/commands/avatar.js
@@ -1,9 +1,8 @@
 exports.output = async ({message, args}) => {
   const user = await Mike.utils.users.search(message, args[0])
-  const member = await message.guild.fetchMember(user)
   Mike.models.mult({
     object: message,
-    description: user.tag,
+    description: `${user.tag} (${user.id})`, 
     image: user.displayAvatarURL,
   })
 }
