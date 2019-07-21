5e8012fc09fab438784a5f75cad10c6fcdcb7687
Nimplex <frekerhugug@gmail.com>
2019-07-21 22:47:39 +0200

    utility plugin

---
+++ plugins/utility/commands/generateinvite.js
@@ -0,0 +1,17 @@
+exports.output = async ({message}) => {
+    const invite = await message.channel.createInvite();
+    return Mike.models.snap({
+      object: message,
+      message: `https://discord.gg/${invite.code}`
+    })
+}
+exports.data = {
+    triggers: ['generateinvite'],
+    description: 'Generates invite for current text channel..',
+    userPerms: [
+      'CREATE_INSTANT_INVITE'
+    ],
+    botPerms: [
+      'CREATE_INSTANT_INVITE'
+    ]
+}
