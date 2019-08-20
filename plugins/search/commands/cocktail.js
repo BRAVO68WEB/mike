9f31904b3ed11cbe9ea5332ba937bf4cc1e7e572
badosz0 <badoszk@gmail.com>
2019-08-20 18:29:10 +0200

    cocktail cleanup

--- plugins/search/commands/cocktail.js
+++ plugins/search/commands/cocktail.js
@@ -1,17 +1,21 @@
 exports.output = async ({message, args}) => {
-  Mike.http.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + args.join(" "))
+  Mike.http.get('https://www.thecocktaildb.com/api/json/v1/1/search.php')
+    .query({
+      s: args.join(" ")
+    })
     .then(async response => {
       const cocktail = response.body
-      if(!cocktail.drinks) return Mike.models.snap({
+      if(!cocktail.drinks) {
+        return Mike.models.snap({
           object: message,
           message: '\`Coctail not found!\`',
           color: '#f44262'
         })
-      else {
+      } else {
         Mike.models.snap({
           object: message,
           message: `**${cocktail.drinks[0].strDrink}**`,
-          image: cocktail.drinks[0].strDrinkThumb,
+          image: cocktail.drinks[0].strDrinkThumb
         })
       }
     })
