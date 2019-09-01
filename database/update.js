ae71ed5cf8cb956f83ef16459250b845d54556f0
badosz0 <badoszk@gmail.com>
2019-09-01 18:23:43 +0200

    delete cache when updating

--- database/update.js
+++ database/update.js
@@ -1,6 +1,9 @@
 const r = require('rethinkdb')
 
 module.exports = async (obj, id, k, v) => {
+    if (obj == 'guilds') {
+      delete Mike.cache.guilds[id]
+    }
     try {
         await r.table(obj)
                 .get(id)
