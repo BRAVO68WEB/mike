b13736519c9a584e657b3eebe929da225fe413c7
badosz0 <badoszk@gmail.com>
2019-09-01 19:39:16 +0200

    members command upgrade

--- plugins/info/commands/members.js
+++ plugins/info/commands/members.js
@@ -4,6 +4,14 @@ exports.output = async ({message}) => {
     return message.guild.members.filter(m => m.presence.status == status && !m.user.bot).size
   }
 
+  function byGame(message, games){
+    let total = 0
+    games.forEach(game => {
+      total += message.guild.members.filter(m => m.presence.game && m.presence.game.name == game && !m.user.bot).size
+    })
+    return total
+  }
+
   const total = message.guild.members.filter(m => !m.user.bot).size
 
   const msg = `**Members (${total})**
@@ -12,6 +20,9 @@ exports.output = async ({message}) => {
   ${Mike.customEmojis.statusDnd} \`${Math.floor(byStatus(message, 'dnd')/total*100)}%\` [ ${byStatus(message, 'dnd')} ] Dnd
   ${Mike.customEmojis.statusOffline} \`${Math.floor(byStatus(message, 'offline')/total*100)}%\` [ ${byStatus(message, 'offline')} ] Offline
 
+  :keyboard: \`${Math.floor(byGame(message, ['JetBrains IDE', 'Atom Editor', 'Visual Studio Code'])/total*100)}%\` [ ${byGame(message, ['JetBrains IDE', 'Atom Editor', 'Visual Studio Code'])} ] Programming
+  :musical_note: \`${Math.floor(byGame(message, ['Spotify'])/total*100)}%\` [ ${byGame(message, ['Spotify'])} ] Listening to Spotify
+  :sleeping_accommodation: \`${Math.floor(message.guild.members.filter(m => !m.presence.game && !m.user.bot).size/total*100)}%\` [ ${message.guild.members.filter(m => !m.presence.game && !m.user.bot).size} ] Doing nothing
   `
   Mike.models.snap({
     object: message,
