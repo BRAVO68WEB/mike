f3e30130a327895e2e88cb0d51df3de9753bbbb0
badosz0 <badoszk@gmail.com>
2019-09-25 20:01:02 +0200

    music cleanup

--- handlers/music/player.js
+++ handlers/music/player.js
@@ -1,75 +1,107 @@
-
 const qc = require("./queue")
 
 const play = async (song, message) => {
-    return new Promise(async (resolve, reject) => {
-        if(!Mike.queue[message.guild.id]) new qc(message.guild.id, Mike)
-        let queue = Mike.queue[message.guild.id]
-        let player = await Mike.player.get(message.guild.id)
-        if (!player) player = await Mike.player.join({
-            guild: message.guild.id,
-            channel: message.member.voiceChannel.id,
-            host: Mike.lavalink.host
-        }, { selfdeaf: true })
-        queue.channel = message.channel.id
-        if(player.playing) {
-            queue.songs.push(song)
-            resolve("queue")
-        } else {
-            player.play(song.track)
-            Object.assign(song, {date: Date.now()})
-            queue.np = song
-            resolve("play")
-        }
+    
+  return new Promise(async (resolve, reject) => {
+        
+    if(!Mike.queue[message.guild.id]) {
+      new qc(message.guild.id)
+    }
 
-        player.once("end", async data => {
-            if(queue.loop) {
-                let song = queue.np
-                song.date = Date.now()
-                song.requester = "Loop"
-                play(song, message)
-                return
-            }
-            if(queue.repeat) {
-                queue.repeat = false
-                let song = queue.np
-                song.date = Date.now()
-                song.requester = "Loop"
-                play(song, message)
-                return
-            }
-            let next = queue.songs.shift()
-            if(next == null) {
-                return
-            } else {
-                setTimeout(() => {
-                    play(next, message)
-                }, 400)
-                let player = await Mike.player.get(message.guild.id)
-                const url = (next.url.startsWith("https://www.youtube.com/") ? `https://i.ytimg.com/vi/${next.url.replace("https://www.youtube.com/watch?v=", "")}/hqdefault.jpg` : ``)
-                Mike.models.snap({
-                  object: message,
-                  message: `Now playing: \`${next.title}\`
-                            from:\`${next.channel}\``,
-                  thumbnail: url,
-                  footer:`🔉 ${player.state.volume}% • Duration: ${await Mike.utils.time.formatLength(next.length) || 'N/A'} • Requester: ${message.author.tag}`
-                })
-            }
-            return
+    let queue = Mike.queue[message.guild.id]
+    let player = await Mike.player.get(message.guild.id)
+        
+    if (!player) {
+      player = await Mike.player.join({
+        guild: message.guild.id,
+        channel: message.member.voiceChannel.id,
+        host: Mike.lavalink.host
+      }, { 
+        selfdeaf: true 
+      })
+    }
+    
+    queue.channel = message.channel.id
+        
+    if(player.playing) {
+              
+      queue.songs.push(song)
+      resolve("queue")
+        
+    } else {
+        
+      player.play(song.track)
+      Object.assign(song, {
+        date: Date.now()
+      })
+      queue.np = song
+      resolve("play")
+    
+    }
+
+    player.once("end", async data => {
+      
+      if(queue.loop) {
+        
+        let song = queue.np
+        song.date = Date.now()
+        song.requester = "Loop"
+        return play(song, message)
+      
+      }
+      
+      if(queue.repeat) {
+
+        queue.repeat = false
+        let song = queue.np
+        song.date = Date.now()
+        song.requester = "Loop"
+        return play(song, message)
+      
+      }
+      
+      let next = queue.songs.shift()
+            
+      if(!next) {
+        return
+      } else {
+        
+        setTimeout(() => {
+            play(next, message)
+        }, 400)
+
+        let player = await Mike.player.get(message.guild.id)
+        const url = (next.url.startsWith("https://www.youtube.com/") ? `https://i.ytimg.com/vi/${next.url.replace("https://www.youtube.com/watch?v=", "")}/hqdefault.jpg` : ``)
+        
+        Mike.models.snap({
+          object: message,
+          message: `Now playing: \`${next.title}\`
+                    from:\`${next.channel}\``,
+          thumbnail: url,
+          footer:`🔉 ${player.state.volume}% • Duration: ${await Mike.utils.time.formatLength(next.length) || 'N/A'} • Requester: ${message.author.tag}`
         })
+      }
+      return
     })
+  })
 }
 
 const getSong = async string => {
-    return new Promise(async (resolve, reject) => {
-        const res = await Mike.http.get(`http://${Mike.lavalink.host}:${Mike.lavalink.port}/loadtracks?identifier=${encodeURIComponent(string)}`)
-        .set("Authorization", Mike.lavalink.password)
-        .catch(err => {
-            console.error(err)
-            return null
-        })
-        if (!res) throw "There was an error, try again"
-        resolve(res.body)
+    
+  return new Promise(async (resolve, reject) => {
+        
+      const res = await Mike.http.get(`http://${Mike.lavalink.host}:${Mike.lavalink.port}/loadtracks?identifier=${encodeURIComponent(string)}`)
+                                 .set("Authorization", Mike.lavalink.password)
+                                 .catch(err => {
+                                    console.error(err)
+                                    return null
+                                  })
+      if (!res) {
+        throw "There was an error, try again"
+      }
+      
+      resolve(res.body)
+    
     })
 }
 
--- handlers/music/queue.js
+++ handlers/music/queue.js
@@ -1,20 +1,34 @@
 module.exports = class queue {
-    constructor(gid) {
-        this.songs = []
-        this.np = {title: "", channel: "", length: 0, requester: "", url: "", track: "", paused: false, pd: 0, date: Date.now()}
-        this.loop = false
-        this.repeat = false
-        this.pause = this.pause
-        this.resume = this.resume
-        Mike.queue[gid] = this
+  
+  constructor(gid) {
+    this.songs = []
+    this.np = {
+      title: "", 
+      channel: "", 
+      length: 0, 
+      requester: "", 
+      url: "", 
+      track: "", 
+      paused: false, 
+      pd: 0, 
+      date: Date.now()
     }
+    this.loop = false
+    this.repeat = false
+    this.pause = this.pause
+    this.resume = this.resume
+      Mike.queue[gid] = this
+    }
+    
     addSong(track, url, title, length, channel, requester) {
         this.songs.push({track, url, title, length, channel, requester})
     }
+
     pause() {
         this.np.paused = true
         this.np.pd = Date.now() - this.np.date
     }
+    
     resume() {
         this.np.date = Date.now() - this.np.pd
         this.np.paused = false
