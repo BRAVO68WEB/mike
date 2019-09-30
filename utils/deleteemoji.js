2abc33f4dbbc634521daa22ecf1e1e8eb1ce7b0e
Badosz <badoszk@gmail.com>
2019-09-30 23:22:37 +0200

    code cleanup

---
+++ utils/deleteemoji.js
@@ -0,0 +1,47 @@
+const discord = require("discord.js")
+
+exports.output = async ({message, args}) => {
+  const emote = discord.Util.parseEmoji(args[0])
+  
+  if (!emote) {
+    return Mike.models.snap({
+      object: message,
+      message: '\`Wrong emoji name.\`',
+      color: '#f44262'
+    })
+  }
+
+  if (!emote.id) {
+    return Mike.models.snap({
+      object: message,
+      message: '\`You can\'t delete this emoji.\`',
+      color: '#f44262'
+    })
+  }
+  
+  message.guild.deleteEmoji(emote.id).then(Mike.models.snap({
+    object: message,
+    message: '\`Emoji deleted.\`',
+  }))
+
+}
+
+exports.data = {
+  triggers: ['deleteemoji'],
+  description: 'Delete emoji by name',
+  usage: [
+    '{prefix}{command} <name>',
+  ],
+  args: [
+    {
+      'type': 'text',
+      'name': 'name'
+    }
+  ],
+  userPerms: [
+    "MANAGE_EMOJIS"
+  ],
+  botPerms: [
+    "MANAGE_EMOJIS"
+  ]
+}
