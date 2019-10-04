986f756f9de3df633180b2abcdd50fd0ac2924fb
Badosz <badoszk@gmail.com>
2019-10-04 15:14:44 +0200

    plugins commands

---
+++ plugins/settings/commands/plugins.js
@@ -0,0 +1,59 @@
+exports.output = async ({message, dbGuild, args}) => {
+  if (args.length >= 2) {
+    if (args[0] == 'enable') {
+      
+      const plugin = Mike.plugins.find(plugin => plugin.id == (args[1] ? args[1].toLowerCase() : '*'))
+
+      if (!plugin) {
+        return Mike.models.snap({
+          object: message,
+          message: '\`Plugin doesn\'t exist.\`',
+          color: '#f44262'
+        })
+      }
+
+      if (!dbGuild.settings.disabledPlugins.includes(args[1])) {
+        return Mike.models.snap({
+          object: message,
+          message: '\`Plugin already enabled.\`',
+          color: '#f44262'
+        })
+      }
+
+      for(let i = 0; i < dbGuild.settings.disabledPlugins.length; i++){
+        if (dbGuild.settings.disabledPlugins[i] === args[1]) {
+          dbGuild.settings.disabledPlugins.splice(i, 1)
+        }
+      }
+
+      await Mike.db.update('guilds', message.guild.id, "settings", dbGuild.settings)
+        
+      return Mike.models.snap({
+          object: message,
+          message: `\`Enabled!\``,
+      })
+    }
+  
+  }
+
+  Mike.models.snap({
+    object: message,
+    message: `Disabled Plugins: \`${dbGuild.settings.disabledPlugins.length ? dbGuild.settings.disabledPlugins.join(', ') : '[none]'}\`
+
+              To change disable/enable plugin, type:
+              \`${Mike.prefix}plugins <disable/enable> <id>\`
+              `,
+  })
+}
+
+  
+exports.data = {
+  triggers: ['plugins'],
+  description: 'Shows plugins settings.',
+  usage: [
+    '{prefix}{command} <disable/enable> <id>'
+  ],
+  userPerms: [
+    "MANAGE_GUILD"
+  ]
+}
