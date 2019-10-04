d8d9438ef2f680b227c0187860a832326245b43a
maanex <maan91315@gmail.com>
2019-10-04 18:05:43 +0200

    yes

--- dashboard/site/public/js/global.js
+++ dashboard/site/public/js/global.js
@@ -9,8 +9,9 @@ Vue.component('gheader', {
                     <div id="header-name-page" v-if="pagename" :style="'--ccolor:'+color" v-html="pagename"></div>
                 </div>
                 <div id="header-nav">
-                    <a class="nav-button" href="https://discord.gg/ZwPfRfp">Support</a>
-                    <a class="nav-button" href="/wip">Commands</a>
+                    <a class="nav-button" href="/team">Team</a>
+                    <a class="nav-button" href="https://discord.gg/hfGSb8y">Support</a>
+                    <a class="nav-button" href="/commands">Commands</a>
                     <a class="nav-button border" href="https://discordapp.com/oauth2/authorize?client_id=419620594645073930&permissions=8&scope=bot">Invite</a>
                 </div>
             </div>
@@ -31,13 +32,13 @@ Vue.component('gfooter', {
                 </div>
                 <div class="footer-column">
                     <h3>Support</h3>
-                    <a href="/wip">FAQ</a>
-                    <a href="https://discord.gg/ZwPfRfp">Support Discord</a>
+                    <a href="/faq">FAQ</a>
+                    <a href="https://discord.gg/hfGSb8y">Support Discord</a>
                     </div>
                     <div class="footer-column">
                     <h3>Legal</h3>
-                    <a href="/wip">Terms</a>
-                    <a href="/wip">Privacy</a>
+                    <a href="/terms">Terms</a>
+                    <a href="/privacy">Privacy</a>
                     <a href="mailto:contact@mikebot.xyz">Send an Email</a>
                 </div>
                 <div class="footer-column">
